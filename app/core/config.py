8ffe5eca3582b9d61f2e256d3d3c6addc9bf8d05
Anurup Majumder <anurupm94@gmail.com>
2026-09-10 23:18:39 +0530

    feat: add environment configuration

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    app_name: str
+    app_version: str
+    environment: str
+    debug: bool
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+
+settings = Settings()
